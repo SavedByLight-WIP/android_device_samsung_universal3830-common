fc778b50e089ce5a47fa850811c976594c5dce9f
Jamie Falconer <mrfluffyoven@gmail.com>
2026-03-21 13:15:13 +0000

    add common.mk

---
+++ common.mk
@@ -0,0 +1,20 @@
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
+
+# Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# sideload
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
