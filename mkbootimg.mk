21a869d77b9b99fa85f8b99d1fa69586a7f73f4f
Jamie Falconer <mrfluffyoven@gmail.com>
2026-03-21 13:22:23 +0000

    Change mkbootimg to universal3830-common

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-MKBOOTIMG := device/samsung/m12/mkbootimg
+MKBOOTIMG := device/samsung/universal3830-common/mkbootimg
 
 FLASH_IMAGE_TARGET ?= $(PRODUCT_OUT)/recovery.tar
 
